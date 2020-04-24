92fe6f414339cfa8e45af76b2d40f58212a24593
Adrian <afluder0147@gmail.com>
2020-04-24 16:59:02 +0200

    Added temperature and changed color spaces

--- src/upng_vs/upng_vs/MSColor.cpp
+++ src/upng_vs/upng_vs/MSColor.cpp
@@ -1,84 +1,86 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "MSColor.h"
 #include "cmath";
 
 /*Convert from RGB to XYZ Color Space*/
 void MSColor::GetXYZFromRGB() 
 {
-	float var_R = (this->R / 255.0f);
-	float var_G = (this->G / 255.0f);
-	float var_B = (this->B / 255.0f);
 
-	if (var_R > 0.04045) {
-		var_R = _CMATH_::pow(((var_R + 0.055) / 1.055),2.4);
-	}
-	else {
-		var_R = var_R / 12.92;
-	}
+	double r = this->R / 255.0;
+	double g = this->G / 255.0;
+	double b = this->B / 255.0;
 
-	if (var_G > 0.04045) {
-		var_G = _CMATH_::pow(((var_G + 0.055) / 1.055), 2.4);
-	}
-	else {
-		var_G = var_G / 12.92;
-	}
+	r = ((r > 0.04045) ? pow((r + 0.055) / 1.055, 2.4) : (r / 12.92)) * 100.0;
+	g = ((g > 0.04045) ? pow((g + 0.055) / 1.055, 2.4) : (g / 12.92)) * 100.0;
+	b = ((b > 0.04045) ? pow((b + 0.055) / 1.055, 2.4) : (b / 12.92)) * 100.0;
 
-	if (var_B > 0.04045) {
-		var_B = _CMATH_::pow(((var_B + 0.055) / 1.055), 2.4);
-	}
-	else {
-		var_B = var_B / 12.92;
-	}
-
-	var_R *= 100.0;
-	var_G *= 100.0;
-	var_B *= 100.0;
-
-	this->X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805;
-	this->Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722;
-	this->Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505;
+	this->X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
+	this->Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
+	this->Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
 }
 
 
 /*Convert from XYZ to RGB Color Space*/
 void MSColor::GetRGBFromXYZ() 
 {
-	float var_X = this->X / 100.0;
-	float var_Y = this->Y / 100.0;
-	float var_Z = this->Z / 100.0;
+	double x = this->X / 100.0;
+	double y = this->Y / 100.0;
+	double z = this->Z / 100.0;
 
-	float var_R = var_X * 3.2406 + var_Y * -1.5372 + var_Z * -0.4986;
-	float var_G = var_X * -0.9689 + var_Y * 1.8758 + var_Z * 0.0415;
-	float var_B = var_X * 0.0557 + var_Y * -0.2040 + var_Z * 1.0570;
+	double r = x * 3.2404542 + y * -1.5371385 + z * -0.4985314;
+	double g = x * -0.9692660 + y * 1.8760108 + z * 0.0415560;
+	double b = x * 0.0556434 + y * -0.2040259 + z * 1.0572252;
 
-	if (var_R > 0.0031308) 
-	{ 
-		var_R = 1.055 * (_CMATH_::pow(var_R, (1 / 2.4))) - 0.055;
-	}
-	else 
-	{
-		var_R = 12.92 * var_R;
-	}
+	r = ((r > 0.0031308) ? (1.055*pow(r, 1 / 2.4) - 0.055) : (12.92*r)) * 255.0;
+	g = ((g > 0.0031308) ? (1.055*pow(g, 1 / 2.4) - 0.055) : (12.92*g)) * 255.0;
+	b = ((b > 0.0031308) ? (1.055*pow(b, 1 / 2.4) - 0.055) : (12.92*b)) * 255.0;
 
-	if (var_G > 0.0031308)
-	{
-		var_G = 1.055 * (_CMATH_::pow(var_G, (1 / 2.4))) - 0.055;
-	}
-	else
-	{
-		var_G = 12.92 * var_G;
-	}
+	this->R = r;
+	this->G = g;
+	this->B = b;
 
-	if (var_B > 0.0031308)
+}
+
+/*Set color and update all color spaces*/
+void MSColor::setColor(float h, float i, float j, BaseColorSpace bcs)
+{
+	if (bcs == RGB)
 	{
-		var_B = 1.055 * (_CMATH_::pow(var_B, (1 / 2.4))) - 0.055;
-	}
-	else
-	{
-		var_B = 12.92 * var_B;
+		this->R = h;
+		this->G = i;
+		this->B = j;
+
+		GetXYZFromRGB();
 	}
 
-	this->R = var_R * 255;
-	this->G = var_G * 255;
-	this->B = var_B * 255;
+	if (bcs == XYZ)
+	{
+		this->X = h;
+		this->Y = i;
+		this->Z = j;
+
+		GetRGBFromXYZ();
+	}
+}
+
+float MSColor::getTemperature()
+{
+	/*normalized chromaticity values*/
+	float x = X / (X + Y + Z);
+	float y = Y / (X + Y + Z);
+
+	float xe = 0.3366;
+	float ye = 0.1735;
+	float a0 = -949.86315;
+	float a1 = 6253.80338;
+	float t1 = 0.92159;
+	float a2 = 28.70599;
+	float t2 = 0.20039;
+	float a3 = 0.00004;
+	float t3 = 0.07125;
+	float n = (x - xe) / (y - ye);
+	
+	float cct = a0 + (a1*_CMATH_::exp(-n / t1)) + (a2*_CMATH_::exp(-n / t2)) + (a3*_CMATH_::exp(-n / t2));
+
+	return cct;
 }
--- src/upng_vs/upng_vs/MSColor.h
+++ src/upng_vs/upng_vs/MSColor.h
@@ -3,31 +3,18 @@
 class MSColor
 {
 	public:
+
 		float R, G, B;			/*RGB Color Space*/
 		float X, Y, Z;			/*XYZ Color Space*/
-		enum BaseColorSpace { RGB, XYZ}baseColorSpace;
 
-		MSColor(float h, float i, float j, BaseColorSpace bcs)
-		{
-			if (bcs == RGB) 
-			{
-				R = h;
-				G = i;
-				B = j;
+		enum BaseColorSpace { RGB, XYZ }baseColorSpace;
 
-				GetXYZFromRGB();
-			}
+		MSColor(float h, float i, float j, BaseColorSpace bcs) { setColor(h, i, j, bcs); }
 
-			if (bcs == XYZ)
-			{
-				X = h;
-				Y = i;
-				Z = j;
-
-				GetRGBFromXYZ();
-			}
-
-		}
 		void GetXYZFromRGB();
 		void GetRGBFromXYZ();
+		void setColor(float h, float i, float j, BaseColorSpace bcs);
+
+		float getTemperature();
+
 };
